85bd6eb4bd508db067e2b63d3fc409e3f60929d7
somewaykaka <han.jian2@northeastern.edu>
2024-04-24 21:01:15 -0700

    update a6 little words

--- Kanbas/courses/dao.js
+++ Kanbas/courses/dao.js
@@ -6,9 +6,6 @@ export const createCourse = async (course) => {
 };
 export const findAllCourses = () => model.find();
 export const findCourseById = (courseId) => model.findById(courseId);
-// export const findUserByUsername = (username) =>  model.findOne({ username: username });
-// export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
-// export const findUsersByRole = (role) => model.find({ role: role });
 export const updateCourse = (courseId, course) =>
   model.updateOne({ _id: courseId }, { $set: course });
 export const deleteCourse = (courseId) => model.deleteOne({ _id: courseId });
--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -1,37 +1,37 @@
-import Database from "../Database/index.js";
+import db from "../Database/index.js";
 
 export default function CourseRoutes(app) {
   app.post("/api/courses", (req, res) => {
     const course = { ...req.body, _id: new Date().getTime().toString() };
-    Database.courses.push(course);
-    res.send(Database.courses);
+    db.courses.push(course);
+    res.send(db.courses);
   });
 
   app.get("/api/courses", (req, res) => {
-    const courses = Database.courses;
+    const courses = db.courses;
     res.send(courses);
   });
 
   app.delete("/api/courses/:id", (req, res) => {
     const { id } = req.params;
-    Database.courses = Database.courses.filter((c) => c._id !== id);
-    res.send(Database.courses);
+    db.courses = db.courses.filter((c) => c._id !== id);
+    res.send(db.courses);
     // res.sendStatus(204);
   });
 
   app.put("/api/courses/:id", (req, res) => {
     const { id } = req.params;
     const course = req.body;
-    Database.courses = Database.courses.map((c) =>
+    db.courses = db.courses.map((c) =>
       c._id === id ? { ...c, ...course } : c
     );
-    res.send(Database.courses);
+    res.send(db.courses);
     // res.sendStatus(204);
   });
 
   app.get("/api/courses/:id", (req, res) => {
     const { id } = req.params;
-    const course = Database.courses.find((c) => c._id === id);
+    const course = db.courses.find((c) => c._id === id);
     if (!course) {
       res.status(404).send("Course not found");
       return;
